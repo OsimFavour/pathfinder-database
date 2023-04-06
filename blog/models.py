208be2f9d4f3868917030577780ab409cdc7a1ec
OsimFavour <famos204@gmal.com>
2023-04-05 22:38:00 -0700

    Modify Database

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
-from blog import db
 from datetime import datetime
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from blog import db, app
 from sqlalchemy.orm import relationship
 from flask_login import UserMixin
 
@@ -24,6 +25,21 @@ class User(UserMixin, db.Model):
     # Comments Parent
     comments = relationship("Comment", back_populates="comment_author") 
 
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config["SECRET_KEY"], expires_sec)
+        return s.dumps({"usser_id": self.id}).decode("utf-8")
+    
+    
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config["SECRET_KEY"])
+        try:
+            user_id = s.loads(token)["user_id"]
+        except: 
+            return None
+        return User.query.gett(user_id)
+    
+    
     def __repr__(self):
         return f"User('{self.name}', '{self.email}', '{self.password}')"
 
