f37da500c69a9e7576a507a4d2dcbe953a11b6bd
Osim Favour <famos204@gmail.com>
2023-04-06 22:58:03 +0100

    Forms

--- blog/forms.py
+++ blog/forms.py
@@ -6,59 +6,8 @@ from blog.models import User
 
 
 ##WTForm
-class CreatePostForm(FlaskForm):
-    title = StringField("Blog Post Title", validators=[DataRequired()])
-    subtitle = StringField("Subtitle", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = CKEditorField("Blog Content", validators=[DataRequired()])
-    submit = SubmitField("Submit Post")
 
  
-class SearchForm(FlaskForm):
-    searched = StringField("Searched", validators=[DataRequired()])
-    
-
-class RegisterForm(FlaskForm):
-    name = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField("Email address", validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
-    submit = SubmitField("Sign Up")
-
-    def validate_username(self, name):
-        user = User.query.filter_by(name=name.data).first()
-        if user:
-            raise ValidationError("That username is taken. Please choose a different one.")
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError("That email is taken. Please choose a different one.")
 
 
-class LoginForm(FlaskForm):
-    email = StringField("Email address", validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    # remember = BooleanField("Remember Me")
-    submit = SubmitField("Sign In")
-
-
-class CommentForm(FlaskForm):
-    comment = CKEditorField("Comments", validators=[DataRequired()], render_kw={"style": "font-weight: bold;"})
-    submit = SubmitField("Submit Comment")
-    
-    
-class RequestResetForm(FlaskForm):
-    email = StringField("Email address", validators=[DataRequired(), Email()])
-    submit = SubmitField("Request Password Reset")
-    
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is None:
-            raise ValidationError("There is no account with that email, you must register first.")
-
-
-class ResetPasswordForm(FlaskForm): 
-    password = PasswordField("Password", validators=[DataRequired()])
-    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
-    submit = SubmitField("Reset Password")
+ 
