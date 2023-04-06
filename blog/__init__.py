175b4ebf842486b9390e95e7ae02d94c205d801a
Osim Favour <famos204@gmail.com>
2023-04-06 22:57:27 +0100

    Blog Init

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_gravatar import Gravatar
-
+from flask_mail import Mail
+from functools import wraps
 
 
 app = Flask(__name__)
@@ -20,6 +22,13 @@ db = SQLAlchemy(app)
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
+app.config["MAIL_SERVER"] = "smtp.googlemail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = os.environ.get("MY_EMAIL")
+app.config["MAIL_PASSWORD"] = os.environ.get("MY_PASSWORD")
+mail = Mail(app)
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 
@@ -28,7 +37,13 @@ from blog import routes
 login_manager.login_view = "login"
 login_manager.login_message_category = "info"
 
-
 gravatar = Gravatar(app, size=100, rating="g", default="retro", force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
+def admin_only(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if current_user.id != 1:
+            return abort(403)
+        return f(*args, **kwargs)
+    return decorated_function
 
