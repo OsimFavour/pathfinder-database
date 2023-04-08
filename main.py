9d031d07f453b14703a26fd6892fe5764507b14d
Osim Favour <famos204@gmail.com>
2023-04-08 16:59:07 +0100

    RESTful blog

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from blog import app
+from blog import create_app
+
+app = create_app()
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', port=5000, debug=True)
-    app.run(host='0.0.0.0', port=5000, debug=True)
-    # app.run(host="http://192.168.31.40:33455/", debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True) 
  
