d262b5893d36b28af8bf73ca3ff06142240d620a
Osim Favour <famos204@gmail.com>
2023-04-24 13:41:25 +0100

    Modify Home Page

--- blog/main/routes.py
+++ blog/main/routes.py
@@ -12,7 +12,6 @@ main = Blueprint("main", __name__)
 
 @main.route('/')
 def home():
-    page = request.args.get("page", 1, type=int)
     posts = PurposePost.query.order_by(PurposePost.date.desc()).limit(4).all()
     fiction_posts = Fiction.query.order_by(Fiction.date.desc()).limit(4).all()
     # return render_template("index.html", all_posts=posts, current_user=current_user)
