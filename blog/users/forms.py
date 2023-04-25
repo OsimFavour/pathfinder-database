b4f8cbbddc9b59c72f16d8fafcafffe6b24dda31
Osim Favour <famos204@gmail.com>
2023-04-25 20:53:59 +0100

    Add EmailSubscriberForm

--- blog/users/forms.py
+++ blog/users/forms.py
@@ -4,6 +4,11 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo ,ValidationError
 
 
+class EmailSubscriberForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    submit = SubmitField("Subscribe")
+    
+
 class RegisterForm(FlaskForm):
     name = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField("Email address", validators=[DataRequired(), Email()])
